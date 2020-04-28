358b9e0dcc875333bdb63c5ff46ada88716af05e
lilxdata <madeline_pignetti@hmc.edu>
2020-04-28 14:00:25 -0700

    starter files for setup to pass data b/t screens

---
+++ goodswimmer/EventCell.swift
@@ -0,0 +1,15 @@
+//
+//  EventCell.swift
+//  goodswimmer
+//
+//  Created by madi on 4/28/20.
+//  Copyright © 2020 madi. All rights reserved.
+//
+
+import Foundation
+
+//TODO
+//sub class either UITableViewCell or UICollectionViewCell
+class EventCell {
+    
+}
---
+++ goodswimmer/EventModel.swift
@@ -0,0 +1,9 @@
+//
+//  EventModel.swift
+//  goodswimmer
+//
+//  Created by madi on 4/28/20.
+//  Copyright © 2020 madi. All rights reserved.
+//
+
+import Foundation
