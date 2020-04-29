08e875d3d2d827e73a40e5bd055df61245735fe1
lilxdata <37276557+lilxdata@users.noreply.github.com>
2020-04-28 20:28:50 -0700

    project architecture outline/todos (#37)
    * starter files for setup to pass data b/t screens
    
    * project architecture and todos

---
+++ goodswimmer/EventModel.swift
@@ -0,0 +1,15 @@
+//
+//  EventModel.swift
+//  goodswimmer
+//
+//  Created by madi on 4/28/20.
+//  Copyright © 2020 madi. All rights reserved.
+//
+
+import Foundation
+
+class EventModel {
+    func getEvents() {}
+    
+    
+}
