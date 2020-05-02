9a38d501f384114c897c753ee1339f220c139351
lilxdata <37276557+lilxdata@users.noreply.github.com>
2020-05-01 22:41:20 -0700

    standardize typography  (#39)
    * add font files to project
    
    * add fonts to Info.plist
    
    * update style helpers with new fonts

---
+++ goodswimmer/View Controllers/SetInterestsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SetInterestsViewController.swift
+//  goodswimmer
+//
+//  Created by madi on 4/28/20.
+//  Copyright © 2020 madi. All rights reserved.
+//
+
+import UIKit
+
+class SetInterestsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
