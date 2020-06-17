3ec954bbe6efc38194dbab4b6a2a32e0bb9ac012
lilxdata <madeline_pignetti@hmc.edu>
2020-06-17 12:09:35 -0700

    add scaffold for Storae Service

---
+++ StorageService.swift
@@ -0,0 +1,15 @@
+//
+//  StorageService.swift
+//  goodswimmer
+//
+//  Created by madi on 6/17/20.
+//  Copyright © 2020 madi. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import FirebaseStorage
+
+struct StorageService {
+    
+}
