cb95c159447550a867e4cb8e2059f957837c33be
lilxdata <37276557+lilxdata@users.noreply.github.com>
2020-07-13 11:20:26 -0400

    image picker, event service, storage service  (#60)
    * onclick for image, pops up imagepicker, still need to handle dealing w/ photo
    
    * add scaffold for Storae Service
    
    * still working on add image func
    
    * event service & storage service implemented, but upload not working
    
    * adding pods...
    
    * update pods

---
+++ StorageService.swift
@@ -0,0 +1,39 @@
+//
+//  StorageService.swift
+//  goodswimmer
+//
+//  Created by madi on 6/17/20.
+//  Copyright © 2020 madi. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import FirebaseStorage
+
+struct StorageService {
+    
+    //method to upload images to FB Storage
+    static func uploadImage(_ image: UIImage, at reference: StorageReference, completion: @escaping (URL?) -> Void) {
+        // change image from UIImage to Data type
+        print("in upload image func")
+        guard let imageData = image.jpegData(compressionQuality: 0.1) else {
+            return completion(nil)
+        }
+        //upload data
+        reference.putData(imageData, metadata: nil, completion: { (metadata, error) in
+            if let error = error {
+                assertionFailure(error.localizedDescription)
+                return completion(nil)
+            }
+            //get URL ref to image
+            reference.downloadURL(completion: { (url, error) in
+                if let error = error {
+                    assertionFailure(error.localizedDescription)
+                    return completion(nil)
+                }
+                completion(url)
+        })
+    })
+    
+}
+}
