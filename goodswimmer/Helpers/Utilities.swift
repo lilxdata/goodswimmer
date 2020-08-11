2c9bd64fc7a57743b0daed78e57513aa8ff0dc66
Alex Echeverria <alexecheverria@alexs-air.attlocal.net>
2020-08-10 21:35:54 -0700

    Moved validators and added city validation

--- goodswimmer/Helpers/Utilities.swift
+++ goodswimmer/Helpers/Utilities.swift
@@ -92,35 +92,4 @@ class Utilities {
         }
         return false
     }
-    
-    /* email validation */
-    static func isEmailValid(_ email: String) -> Bool {
-        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
-        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
-        return emailPred.evaluate(with: email)
-        
-    }
-    /* name validation */
-    static func isNameValid(_ name: String) -> Bool {
-        let nameRegEx = "/^[A-Za-z][A-Za-z\'\\-]+([\\ A-Za-z][A-Za-z\'\\-]+)*/"
-        let namePred = NSPredicate(format:"SELF MATCHES %@", nameRegEx)
-        return namePred.evaluate(with: name)
-        
-    }
-    
-    /* username validation */
-    static func isUsernameValid(_ username: String) -> Bool {
-        let usernameRegEx = "^[a-zA-Z0-9_-]+"
-        let usernamePred = NSPredicate(format:"SELF MATCHES %@", usernameRegEx)
-        return usernamePred.evaluate(with: username)
-        
-    }
-    
-    /* date validation */
-    static func isDateValid(_ date: String) -> Bool {
-        let usernameRegEx = "^(0[1-9]|1[012])\\/(0[1-9]|[12][0-9]|3[01])\\/(19|20)\\d\\d ([01][0-9]|2[0-3]):([0-5][0-9])$"
-        let usernamePred = NSPredicate(format:"SELF MATCHES %@", usernameRegEx)
-        return usernamePred.evaluate(with: date)
-        
-    }
 }
