086e1d5e9d8ffbb8bf1b8fca39f3efc0959019d4
Alex Echeverria <alexecheverria@alexs-air.attlocal.net>
2020-08-16 21:04:55 -0700

    Updating registering by using validators; use search bar in order to filter

--- goodswimmer/View Controllers/SearchViewController.swift
+++ goodswimmer/View Controllers/SearchViewController.swift
@@ -13,9 +13,17 @@ class SearchViewController: UIViewController {
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet var filterButton: UIButton!
     @IBAction func filterResults(_ sender: UIButton) {
-        filterResultsArray(filter: "Monster giveaway", category: "name")
+        print("I am using the button")
+        filterSearchBar(searchBar, textDidChange:searchBar.text!)
     }
-    func filterResultsArray(filter: String, category : String) {
+    
+    // This method updates filteredData based on the text in the Search Box
+    func filterSearchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print("I am using ", searchText)
+        filterResultsArray(filter:searchText, category:"name")
+    }
+
+    func filterResultsArray(filter: String, category : String) { 
         for event in eventArray.events {
             if(category == "name"){
                 if(event.name?.contains(filter) == true){
@@ -37,6 +45,6 @@ class SearchViewController: UIViewController {
 
         // Do any additional setup after loading the view.
         filterResults(filterButton)
-        eventService.filterEvents(filter: "luhmow", cat: "name")
+        
     }
 }
