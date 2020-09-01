049086cf441d2eeb145132871f1b9864ebd51ad0
Alex Echeverria <alexecheverria@alexs-air.attlocal.net>
2020-08-31 19:06:07 -0700

    Added search bar that updates when inputs are typed

--- goodswimmer/View Controllers/SearchViewController.swift
+++ goodswimmer/View Controllers/SearchViewController.swift
@@ -9,42 +9,66 @@
 import UIKit
 
 class SearchViewController: UIViewController {
-
-    @IBOutlet weak var searchBar: UISearchBar!
-    @IBOutlet var filterButton: UIButton!
-    @IBAction func filterResults(_ sender: UIButton) {
-        print("I am using the button")
-        filterSearchBar(searchBar, textDidChange:searchBar.text!)
-    }
-    
-    // This method updates filteredData based on the text in the Search Box
-    func filterSearchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        print("I am using ", searchText)
-        filterResultsArray(filter:searchText, category:"name")
-    }
-
-    func filterResultsArray(filter: String, category : String) { 
-        for event in eventArray.events {
-            if(category == "name"){
-                if(event.name?.contains(filter) == true){
-                    print(event)
-                }
-            }
-        }
-    }
-    
-    
+    var searchController: UISearchController!
     
     //access to eventarray across app
     let eventArray = EventArray.sharedInstance
     let eventService = EventService()
     
+    @IBOutlet var searchContainerView: UIView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-
-        // Do any additional setup after loading the view.
-        filterResults(filterButton)
+        searchController = UISearchController(searchResultsController: nil)
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchContainerView.addSubview(searchController.searchBar)
+        searchController.searchBar.delegate = self
+        searchController.searchBar.placeholder = "Event Filter"
         
+    }
+    
+    func filterEvents(_ searchTerm: String){
+        print(searchTerm)
+        for event in eventArray.events {
+            print(event)
+            if(event.name == searchTerm){
+                print(event)
+            }
+        }
+    }
+    
+     @IBAction func filterButtonPress(_ sender: Any) {
+        print("I am using the button")
+        //print(isSearchBarEmpty)
+        //print(searchBar.text!)
+        //print(searchController.searchBar.text!)
+        //filterSearchBar(searchBar, textDidChange:searchBar.text!)
+    }
+
+}
+
+extension SearchViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        if let searchText = searchController.searchBar.text {
+            filterEvents(searchText)
+        }
+    }
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        if let searchText = searchController.searchBar.text {
+            filterEvents(searchText)
+        }
+    }
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchController.isActive = false
+        if let searchText = searchBar.text, !searchText.isEmpty {
+            searchBar.text = ""
+        }
     }
 }
