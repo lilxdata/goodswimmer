6a36d7e77a6c6def0dff8c7987d0e0f1d5fb1818
lilxdata <37276557+lilxdata@users.noreply.github.com>
2020-09-28 12:30:19 -0400

    add labels and icons to DV (#83)

--- goodswimmer/View Controllers/DetailViewController.swift
+++ goodswimmer/View Controllers/DetailViewController.swift
@@ -14,10 +14,11 @@ class DetailViewController: UIViewController {
     // @IBOutlet weak var eventHeader: UILabel!
     @IBOutlet weak var eventTitle: UILabel?
     @IBOutlet weak var eventImage: UIImageView?
-    @IBOutlet weak var eventLocation: UILabel?
     @IBOutlet weak var eventDate: UILabel?
     @IBOutlet weak var eventTime: UILabel?
+    @IBOutlet weak var eventLocation: UILabel?
     @IBOutlet weak var eventDesc: UILabel?
+    
     
     var selectedEvent: Event?
     
@@ -32,14 +33,14 @@ class DetailViewController: UIViewController {
             let location = event.venue ?? Constants.Placeholders.placeholderText
             let date = event.dateStart ?? Constants.Placeholders.placeholderText
             let time = event.time ?? Constants.Placeholders.placeholderText
-            let desc = event.description ?? Constants.Placeholders.placeholderText
+            let description = event.description ?? Constants.Placeholders.placeholderText
             
             eventTitle?.text = title
             eventLocation?.text = location
             eventImage?.sd_setImage(with: photoURL, completed: nil)
             eventDate?.text = date
             eventTime?.text = time
-            eventDesc?.text = desc
+            eventDesc?.text = description
         }
         
     }
@@ -49,7 +50,22 @@ class DetailViewController: UIViewController {
         self.dismiss(animated: true, completion: nil)
     }
     
-    /* to do: unwrap optionals / error handle */
+    
+    @IBAction func addToList(_ sender: Any) {
+        print("add to list button tapped")
+    }
+    
+    
+    @IBAction func viewButtonTapped(_ sender: Any) {
+        print("view button tapped")
+    }
+    
+    
+    @IBAction func followButtonTapped(_ sender: Any) {
+        print("follow button tapped")
+    }
+    
+    /* TODO: unwrap optionals!!!! / error handle */
     func setUpElements() {
         
         let eventHeader = navBar.topItem?.title ?? "EVENT"
@@ -57,10 +73,10 @@ class DetailViewController: UIViewController {
    //     Utilities.styleLabel(eventHeader, size: 24, uppercase: true)
         Utilities.styleLabel(eventTitle!, size: 35, uppercase: false)
 //        Utilities.styleLabel(eventHeader, size: 15, uppercase: true)
-//        Utilities.styleLabel(eventLocation, size: 15, uppercase: false)
-//        Utilities.styleLabel(eventDate, size: 24, uppercase: false)
-//        Utilities.styleLabel(eventTime, size: 24, uppercase: false)
-      Utilities.styleLabel(eventDesc!, size: 15, uppercase: false)
+        Utilities.styleLabel(eventLocation!, size: 15, uppercase: false)
+    //    Utilities.styleLabel(eventDate ?? "lala", size: 24, uppercase: false)
+   //     Utilities.styleLabel(eventTime!, size: 24, uppercase: false)
+//        Utilities.styleLabel(eventDesc ?? "nil", size: 15, uppercase: false)
     }
     
 }
