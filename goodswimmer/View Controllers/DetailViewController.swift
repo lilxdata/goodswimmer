c457589d7227daa46a03eb5506beedf27b1448a5
lilxdata <37276557+lilxdata@users.noreply.github.com>
2020-09-30 16:46:51 -0400

    fix scroll view (#86)

--- goodswimmer/View Controllers/DetailViewController.swift
+++ goodswimmer/View Controllers/DetailViewController.swift
@@ -12,12 +12,12 @@ class DetailViewController: UIViewController {
     
     @IBOutlet weak var navBar: UINavigationBar!
     // @IBOutlet weak var eventHeader: UILabel!
-    @IBOutlet weak var eventTitle: UILabel?
-    @IBOutlet weak var eventImage: UIImageView?
-    @IBOutlet weak var eventDate: UILabel?
-    @IBOutlet weak var eventTime: UILabel?
-    @IBOutlet weak var eventLocation: UILabel?
-    @IBOutlet weak var eventDesc: UILabel?
+    @IBOutlet weak var eventTitle: UILabel!
+    @IBOutlet weak var eventImage: UIImageView!
+    @IBOutlet weak var eventDate: UILabel!
+    @IBOutlet weak var eventTime: UILabel!
+    @IBOutlet weak var eventLocation: UILabel!
+    @IBOutlet weak var eventDesc: UILabel!
     
     
     var selectedEvent: Event?
@@ -26,22 +26,22 @@ class DetailViewController: UIViewController {
         super.viewDidLoad()
         
         setUpElements()
-       
+        
         if let event = selectedEvent {
             let photoURL = URL(string: event.photoURL ?? Constants.Placeholders.placeholderURL)
             let title = event.name ?? Constants.Placeholders.placeholderText
             let location = event.venue ?? Constants.Placeholders.placeholderText
             let date = event.startDate!
-                //TODO: make a timestamp obj placeholder for optional unwrapping ?? Constants.Placeholders.placeholderText
+            //TODO: make a timestamp obj placeholder for optional unwrapping ?? Constants.Placeholders.placeholderText
             let time = event.time ?? Constants.Placeholders.placeholderText
             let description = event.description ?? Constants.Placeholders.placeholderText
             
-            eventTitle?.text = title
-            eventLocation?.text = location
-            eventImage?.sd_setImage(with: photoURL, completed: nil)
-         //   eventDate?.text = date
-            eventTime?.text = time
-            eventDesc?.text = description
+            eventTitle.text = title
+            eventLocation.text = location
+            eventImage.sd_setImage(with: photoURL, completed: nil)
+            //   eventDate?.text = date
+            eventTime.text = time
+            eventDesc.text = description
         }
         
     }
@@ -66,18 +66,15 @@ class DetailViewController: UIViewController {
         print("follow button tapped")
     }
     
-    /* TODO: unwrap optionals!!!! / error handle */
     func setUpElements() {
         
         let eventHeader = navBar.topItem?.title ?? "EVENT"
         Utilities.styleEventHeader(eventHeader)
-   //     Utilities.styleLabel(eventHeader, size: 24, uppercase: true)
-        Utilities.styleLabel(eventTitle!, size: 35, uppercase: false)
-//        Utilities.styleLabel(eventHeader, size: 15, uppercase: true)
-        Utilities.styleLabel(eventLocation!, size: 15, uppercase: false)
-    //    Utilities.styleLabel(eventDate ?? "lala", size: 24, uppercase: false)
-   //     Utilities.styleLabel(eventTime!, size: 24, uppercase: false)
-//        Utilities.styleLabel(eventDesc ?? "nil", size: 15, uppercase: false)
+        Utilities.styleLabel(eventTitle, size: 35, uppercase: false)
+        Utilities.styleLabel(eventLocation, size: 15, uppercase: false)
+        Utilities.styleLabel(eventDate, size: 24, uppercase: false)
+        Utilities.styleLabel(eventTime, size: 24, uppercase: false)
+        Utilities.styleLabel(eventDesc, size: 15, uppercase: false)
     }
     
 }
