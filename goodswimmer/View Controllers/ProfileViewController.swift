c1d6808ec98e79f674aa8dc0585e0029db49d44f
Alex Echeverria <alexecheverria@alexs-air.attlocal.net>
2020-10-07 01:46:58 -0700

    Updated photo retrieval in sign up

--- goodswimmer/View Controllers/ProfileViewController.swift
+++ goodswimmer/View Controllers/ProfileViewController.swift
@@ -13,16 +13,30 @@ import FirebaseAuth
 import FirebaseStorage
 
 
-//https://firebasestorage.googleapis.com/v0/b/good-swimmer.appspot.com/o/test_image.jpg?alt=media&token=7ab711db-332a-4cf1-a4b3-bb711b0def0c
-
 class ProfileViewController: UIViewController {
 
     @IBOutlet weak var profileImage: UIButton!
     @IBAction func profileImageTapped(_ sender: Any) {
+        
         photoHelper.completionHandler =  { image in
-            self.eventService.uploadImage(for: image, id: self.uuid)
+            //make unique identifier for image
+            let photoid = UUID().uuidString
+            let imageRef = Storage.storage().reference().child(photoid+".jpg")
+            StorageService.uploadImage(image, at: imageRef) { (downloadURL) in
+                guard let downloadURL = downloadURL else {
+                    return
+                }
+                let urlString = downloadURL.absoluteString
+                print("image URL: \(urlString)")
+                let changeRequest = Auth.auth().currentUser?.createProfileChangeRequest()
+                changeRequest?.photoURL = URL(string: urlString)
+                changeRequest?.commitChanges { (error) in
+                }
+            }
         }
         photoHelper.presentActionSheet(from: self)
+        let user = Auth.auth().currentUser
+        profileImage.sd_setImage(with: user?.photoURL, for: state, completed: nil)
     }
     
     let eventService = EventService.sharedInstance
@@ -40,53 +54,25 @@ class ProfileViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Create a child reference
-        // imagesRef now points to "images"
-        let imagesRef = storageRef.child("images")
-
-        // Child references can also take paths delimited by '/'
-        // spaceRef now points to "images/space.jpg"
-        // imagesRef still points to "images"
-        var profRef = storageRef.child("goodswimmer stock profile.jpg")
-
-        print(profRef)
-
-        
-        let imageURL = URL(string: "https://firebasestorage.googleapis.com/v0/b/good-swimmer.appspot.com/o/goodswimmer%20stock%20profile.png?alt=media&token=ca665fa0-9b4e-491d-bdfd-4195752d4dea")
-        profileImage.sd_setImage(with: imageURL, for: state, completed: nil)
-        let db = Firestore.firestore()
-        //var users = db.collection("users")
-        db.collection("users").getDocuments() { (querySnapshot, err) in
-            if let err = err {
-                print("Error getting documents: \(err)")
-            } else {
-                for document in querySnapshot!.documents {
-                    print("\(document.documentID) => \(document.data())")
-                }
-            }
+        let user = Auth.auth().currentUser
+        profileImage.sd_setImage(with: user?.photoURL, for: state, completed: nil)
+        profileImage.imageView?.makeRounded()
+        let stockPhotoRef = storageRef.child("goodswimmer stock profile.png")
+        // Fetch the download URL
+        stockPhotoRef.downloadURL { url, error in
+          if let error = error {
+            // Handle any errors
+            print("Error retreiving stock photo")
+            print(error)
+          } else {
+            // Get the download URL
+            //Setting profile pic to default swimming pic in firebase
+            //changeRequest?.photoURL = url
+            print(URL(string: url!.absoluteString))
+            //changeRequest?.photoURL = URL(string: url?.absoluteString)
+          }
         }
-       
-
-        //print("I am doing stuff")
-        /*db.collection("events").order(by: "start_date").addSnapshotListener { (querySnapshot, error) in
-            if error == nil && querySnapshot != nil {
-                //clear event array to remove dupes
-                self.eventArray.events.removeAll()
-                for document in querySnapshot!.documents {
-                    print("document received")
-                    let eventData = document.data()
-                    if let event = Event(eventDict: eventData) {
-                        print("I am pulling from firebase?")
-                        self.eventArray.events.append(event)
-                    }
-                    
-                }
-            }
-        }*/
-        print("I am rounding the image")
-        //image!.makeRounded()
-        // Do any additional setup after loading the view.
+      
     }
     
     //TODO: set user as not logged in...?
@@ -118,10 +104,12 @@ extension UIImageView {
 
     func makeRounded() {
 
-        self.layer.borderWidth = 1
+        self.layer.borderWidth = 3
         self.layer.masksToBounds = false
         self.layer.borderColor = UIColor.black.cgColor
-        self.layer.cornerRadius = self.frame.height / 2
+        print("I am printing height")
+        print(self.frame.height)
+        self.layer.cornerRadius = 40.0 //self.frame.height / 1
         self.clipsToBounds = true
     }
 }
